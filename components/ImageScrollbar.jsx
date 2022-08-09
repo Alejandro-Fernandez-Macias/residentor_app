9b4b207af1124f7a93ef43fbf60e678684cfd950
Alejandro Fernandez Macias <afpato3@gmail.com>
2022-08-09 12:52:34 -0700

    updated

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
-import Image from "next/image";
-import { Box, Icon, Flex } from "@chakra-ui/react";
-import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
+import { useContext } from 'react';
+import Image from 'next/image';
+import { Box, Icon, Flex } from '@chakra-ui/react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
 const LeftArrow = () => {
     const { scrollPrev } = useContext(VisibilityContext);
@@ -35,16 +35,14 @@ const RightArrow = () => {
         </Flex>
     );
 }
-const ImageSrollbar = ({ data }) => {
+export default function ImageSrollbar({ data }) {
     return (
         <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
             {data.map((item) => (
                 <Box width='910px' itemId={item.id} overflow='hidden' p='1'>
-                    <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500} sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px" alt="property image" />
+                    <Image placeholder="blur" blurDataURL={item.url} src={item.url} width={1000} height={500} sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"  alt='proprty image'/>
                 </Box>
             ))}
         </ScrollMenu>
     );
 }
-
-export default ImageSrollbar;
